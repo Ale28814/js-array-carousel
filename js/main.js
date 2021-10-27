dab191e72c8eff9af4e6c126cf96728cfc9ea374
Ale28814 <alened928@gmail.com>
2021-10-27 15:55:23 +0200

    add exc

--- js/main.js
+++ js/main.js
@@ -39,4 +39,73 @@ const textCollection = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
+//DECLARATION
+const imagesRef = document.querySelector('.images');
+const ithumbsRef = document.querySelector('.thumbs');
 
+let activeImage = 1;
+
+for (let i = 0; i < imageCollection.length; i++) {
+
+    //IMAGES
+    imagesRef.innerHTML += `<div class="image-container">
+    <img src="${imageCollection[i]}" alt="${titleCollection[i]}">
+    <div class="text">
+        <h3>${titleCollection[i]}</h3>
+        <p>${textCollection[i]}</p>
+    </div>
+    </div>`;
+
+    //THUMB
+    ithumbsRef.innerHTML += `<div class="thumb">
+    <img src="${imageCollection[i]}" alt="${titleCollection[i]}">
+    </div>`;
+}
+
+console.log(document.getElementsByClassName('image-container') );
+document.getElementsByClassName('image-container')[activeImage].classList.add('active');
+
+console.log(document.getElementsByClassName('thumb') );
+document.getElementsByClassName('thumb')[activeImage].classList.add('active');
+
+//PROX IMG
+const next = document.querySelector('.next');
+
+next.addEventListener('click', function() {
+    //ACTIVE IMG
+    if(activeImage === imageCollection.length - 1) {
+        activeImage = 0;
+    } else {
+        activeImage++;
+    }
+
+    //SET ACTIVE IMG
+    document.querySelector('.image-container.active').classList.remove('active');
+    //NEXT ACTIVE IMG
+    document.getElementsByClassName('image-container')[activeImage].classList.add('active');
+    //SET THUMB
+    document.querySelector('.thumb.active').classList.remove('active');
+
+    document.getElementsByClassName('thumb')[activeImage].classList.add('active');
+})
+
+//PREVIOUS
+const prev = document.querySelector('.prev');
+
+prev.addEventListener('click', function() {
+    //ACTIVE IMG
+    if(activeImage === imageCollection.length - 5) {
+        activeImage = 4;
+    } else {
+        activeImage--;
+    }
+
+    //SET ACTIVE IMG
+    document.querySelector('.image-container.active').classList.remove('active');
+    //NEXT ACTIVE IMG
+    document.getElementsByClassName('image-container')[activeImage].classList.add('active');
+    //SET THUMB
+    document.querySelector('.thumb.active').classList.remove('active');
+
+    document.getElementsByClassName('thumb')[activeImage].classList.add('active');
+})
